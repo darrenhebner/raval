4891f9abc433d10c17d3539d80cb8eec9b7586c5
Darren Hebner <darrenwilliamhebner@gmail.com>
2026-02-02 15:22:02 -0500

    Adds typesafe contexts

--- apps/demo/server.ts
+++ apps/demo/server.ts
@@ -1,7 +1,33 @@
-import { greeting } from "streamweaver";
+import { createContext, Route } from "streamweaver";
+
+interface Data {
+  name: string;
+}
+
+const DataContext = createContext<Data>();
+
+interface Params {
+  id: number;
+}
+
+const ParamsContext = createContext<Params>();
+
+function* app() {
+  const { name } = yield* DataContext;
+  const { id } = yield* ParamsContext;
+  return `<h1>hi, ${name}. Id: ${id}</h1>`;
+}
 
 export default {
-  async fetch(request: Request) {
-    return new Response(greeting());
+  async fetch() {
+    const route = new Route(app)
+      .setContext(DataContext, { name: "bill" })
+      .setContext(ParamsContext, { id: 123 });
+
+    return new Response(route.renderToStream(), {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
   },
 } satisfies ExportedHandler<Cloudflare.Env>;
--- packages/streamweaver/index.ts
+++ packages/streamweaver/index.ts
@@ -1,3 +1,63 @@
-export function greeting() {
-  return "Hello from StreamWeaver!";
+export interface Context<T> {
+  [Symbol.iterator](): Generator<Context<T>, T, unknown>;
+}
+
+export function createContext<T>(): Context<T> {
+  const context = {
+    *[Symbol.iterator](): Generator<Context<T>, T, unknown> {
+      return (yield context) as T;
+    },
+  };
+  return context;
+}
+
+export class Route<Yields = never> {
+  #context = new Map<unknown, unknown>();
+  #app: () => Generator<Yields, string, unknown>;
+
+  constructor(app: () => Generator<Yields, string, unknown>) {
+    this.#app = app;
+  }
+
+  setContext<T>(context: Context<T>, value: T) {
+    this.#context.set(context, value);
+    return this as unknown as Route<Exclude<Yields, Context<T>>>;
+  }
+
+  renderToStream(this: Route<never>) {
+    const encoder = new TextEncoder();
+    const generator = this.#app();
+    const contextMap = this.#context;
+
+    return new ReadableStream({
+      start(controller) {
+        try {
+          let nextInput: unknown;
+          let result = generator.next(nextInput);
+
+          while (!result.done) {
+            const value = result.value;
+
+            if (contextMap.has(value)) {
+              nextInput = contextMap.get(value);
+            } else {
+              if (typeof value === "string") {
+                controller.enqueue(encoder.encode(value));
+              }
+              nextInput = undefined;
+            }
+
+            result = generator.next(nextInput);
+          }
+
+          if (typeof result.value === "string") {
+            controller.enqueue(encoder.encode(result.value));
+          }
+          controller.close();
+        } catch (e) {
+          controller.error(e);
+        }
+      },
+    });
+  }
 }
