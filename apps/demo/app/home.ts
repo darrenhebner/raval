e13e38d569db4f3b01b60c973bfb302e6c49a8cf
Darren Hebner <darrenwilliamhebner@gmail.com>
2026-02-05 08:59:10 -0500

    Some basic styling for the home page

--- apps/demo/app/home.ts
+++ apps/demo/app/home.ts
@@ -1,38 +1,69 @@
 import { css, html } from "streamweaver";
 import { FeedContext } from "../shared/feed";
 import type { Release } from "../shared/types";
-import { routes } from "./routes";
+import { ResetCss, ThemeCss } from "../shared/styles";
 
 const ReleaseItemCss = css`
-  li {
-    color: blue;
+  .ReleaseItemTrack {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 12px;
+  }
+
+  .ReleaseItemArtwork {
+    border-radius: 24px;
+    corner-shape: squircle;
+  }
+
+  .ReleaseItemReviews {
+    list-style: none;
+    font-size: 14px;
+    padding-left: 12px;
+  }
+
+  .ReleaseItemReviews > :not(:last-child) {
+    margin-bottom: 12px;
+  }
+
+  .ReleaseItemReviewHeading {
+    display: flex;
+    align-items: center;
+    gap: 4px;
   }
 `;
 
-function* ReleaseItem({ artists, artworkUrl, title, mbid, reviews }: Release) {
+function* ReleaseItem({ artists, artworkUrl, title, reviews }: Release) {
   yield ReleaseItemCss;
 
   yield* html`<li>
-    ${artworkUrl
-      ? html`<img
-          src="${artworkUrl}"
-          alt="${title}"
-          width="100"
-          height="100"
-        />`
-      : ""}
-    ${title} - ${artists.map((artist) => artist.name).join(", ")}
-    <a href="https://musicbrainz.org/release/${mbid}">Musicbrainz</a>
-    <a href="${routes.release.href({ mbid: mbid })}">View</a>
-    <ul>
+    <div class="ReleaseItemTrack">
+      <div>
+        <h3>${title}</h3>
+        <p>${artists.map((artist) => artist.name).join(", ")}</p>
+      </div>
+      ${artworkUrl
+        ? html`<img
+            class="ReleaseItemArtwork"
+            src="${artworkUrl}"
+            alt="${title}"
+            width="60"
+            height="60"
+          />`
+        : ""}
+    </div>
+    <ul class="ReleaseItemReviews">
       ${reviews.map(
         (review) =>
-          html`<li>
-            <img
-              src="https://www.google.com/s2/favicons?domain=${review
-                .publication.url}&sz=16"
-            />
-            ${review.publication.name} <a href="${review.url}">Visit</a>
+          html`<li class="ReleaseItemReview">
+            <h4 class="ReleaseItemReviewHeading">
+              <img
+                height="16"
+                width="16"
+                src="https://www.google.com/s2/favicons?domain=${review
+                  .publication.url}&sz=16"
+              />
+              <a href="${review.url}">${review.publication.name}</a>
+            </h4>
             ${review.snippet
               ? html`<blockquote>${review.snippet}</blockquote>`
               : ""}
@@ -42,10 +73,32 @@ function* ReleaseItem({ artists, artworkUrl, title, mbid, reviews }: Release) {
   </li>`;
 }
 
+const HomeCss = css`
+  .Container {
+    max-width: 600px;
+    margin: 16px auto;
+    width: 90%;
+  }
+
+  .Feed {
+    list-style: none;
+  }
+
+  .Feed > :not(:last-child) {
+    margin-bottom: 24px;
+  }
+`;
+
 export function* Home() {
+  yield ResetCss;
+  yield ThemeCss;
+  yield HomeCss;
+
   const { releases } = yield* FeedContext;
 
-  yield* html`<ul>
-    ${releases.map((release) => html`<${ReleaseItem} ...${release} />`)}
-  </ul>`;
+  yield* html`<main class="Container">
+    <ol class="Feed">
+      ${releases.map((release) => html`<${ReleaseItem} ...${release} />`)}
+    </ol>
+  </main>`;
 }
